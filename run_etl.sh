2e61b55a6477871ebd8bac373820cd12ac38db94
Felipe Talavera <felipe@talavera.dev>
2020-09-27 21:01:35 +0000

    remove pg env vars

--- run_etl.sh
+++ run_etl.sh
@@ -5,10 +5,4 @@ export OURA_CLIENT_ID='xxxxxxxx';
 export OURA_CLIENT_SECRET='xxxxxxxx';
 export REDIRECT_URI='https://xxxxxxxx.com/';
 
-export DB_USER='';
-export DB_PASSWORD='';
-export DB_HOST='';
-export DB_PORT='';
-export DB_DATABASE='';
-
 python main.py;
